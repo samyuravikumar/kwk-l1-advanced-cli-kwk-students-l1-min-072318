581f0b86fa2c194b91a7640b3e5b9b3bc3e74f19
samyuravikumar <samyuktha321@gmail.com>
2018-07-24 14:10:26 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -24,3 +24,10 @@ mv delivery/oreos.txt snax;
 mv delivery/potatoes.txt veggies;
 mv delivery/salmon_steak.txt meat;
 mv delivery/tiramisu_cake.txt dessert;
+mv delivery/tomatoes.txt fruit;
+mv delivery/trail_mix.txt snax;
+mv delivery/turkey_breast.txt meat;
+mv delivery/velveeta_cheese.txt dairy;
+mv delivery/watermelon.txt fruit;
+mv delivery/whole_chicken.txt meatl
+mv delivery/wonder_bread.txt 

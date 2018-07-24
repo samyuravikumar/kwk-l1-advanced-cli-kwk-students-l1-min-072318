3c42bf161b58aa6351c09a6493abb4e4f673eaaf
samyuravikumar <samyuktha321@gmail.com>
2018-07-24 14:10:36 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -29,5 +29,5 @@ mv delivery/trail_mix.txt snax;
 mv delivery/turkey_breast.txt meat;
 mv delivery/velveeta_cheese.txt dairy;
 mv delivery/watermelon.txt fruit;
-mv delivery/whole_chicken.txt meatl
-mv delivery/wonder_bread.txt 
+mv delivery/whole_chicken.txt meat;
+mv delivery/wonder_bread.txt snax;
